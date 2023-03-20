fc63456b3b3ecacfd2151f4a7c3e84d4ef2e09ff
diget611 <diget611@naver.com>
2023-03-20 13:49:02 +0900

    exception 처리
    오류 페이지 이동

--- src/main/java/kh/spring/chazazo/member/controller/MemberController.java
+++ src/main/java/kh/spring/chazazo/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package kh.spring.chazazo.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,18 @@ public class MemberController {
 	@PostMapping("/register")
 	public ModelAndView register(ModelAndView mv, MemberDto memberDto, MemberInfoDto memberInfoDto) {
 		service.insert(memberDto, memberInfoDto);
+		return mv;
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView memberExceptionHandler(Exception e
+			/* 오류발생함 ModelAndView mv */) {
+		e.printStackTrace();
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("msg", e.getMessage() + "오류가 발생했습니다. 다시 시도해주세요.");
+		mv.setViewName("error/error500");
+		
 		return mv;
 	}
 

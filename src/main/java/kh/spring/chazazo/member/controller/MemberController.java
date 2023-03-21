28fdbb23583c61c0ea190c79b9e34afae6614020
diget611 <diget611@naver.com>
2023-03-21 14:19:01 +0900

    아이디 중복 체크 기능 작성 및 가입 후 리다이렉트 작성

--- src/main/java/kh/spring/chazazo/member/controller/MemberController.java
+++ src/main/java/kh/spring/chazazo/member/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.google.gson.Gson;
+
 import kh.spring.chazazo.member.model.dto.MemberReqDto;
 import kh.spring.chazazo.member.model.dto.MemberInfoReqDto;
 import kh.spring.chazazo.member.model.service.MemberService;
@@ -18,74 +20,74 @@ import kh.spring.chazazo.member.model.service.MemberService;
 @RestController
 @RequestMapping("/member")
 public class MemberController {
-	
+
 	@Autowired
 	private MemberService mService;
-	
+
 	@GetMapping
 	public ModelAndView viewMemberList(ModelAndView mv) {
 		// 관리자 회원 리스트 조회
 		return mv;
 	}
-	
+
 //	@GetMapping
 	public ModelAndView viewMemberOne(ModelAndView mv, String username) {
 		// 회원 상세 조회
 		return mv;
 	}
-	
+
 	@GetMapping("/register")
 	public ModelAndView viewInsertMember(ModelAndView mv) {
 		mv.setViewName("member/register");
 		return mv;
 	}
-	
+
 	@PostMapping("/register")
 	public ModelAndView insertMember(ModelAndView mv, MemberReqDto memberDto, MemberInfoReqDto memberInfoDto) {
-		mService.insert(memberDto, memberInfoDto);
+		int result = mService.insert(memberDto, memberInfoDto);
+		if (result > 0) {
+			mv.setViewName("redirect:/member/login");
+		} else {
+			// TODO : 가입실패시 처리할 방법 작성해야 함
+			mv.setViewName("redirect:/");
+		}
 		return mv;
 	}
-	
-	@GetMapping("/register/{username}")
-	public String checkDup(@PathVariable String username) {
+
+	@GetMapping("/register/exist")
+	public int checkDup(String username) {
 		int result = mService.checkDup(username);
-		if(result == 1) {
-			
-		} else {
-			
-		}
-		return null;
+		return result;
 	}
-	
+
 //	@GetMapping
 	public ModelAndView viewUpdateMember(ModelAndView mv) {
 		// 회원정보 수정 페이지 조회
 		return mv;
 	}
-	
+
 //	@PostMapping
 	public ModelAndView updateMember(ModelAndView mv, String password, MemberInfoReqDto dto) {
-		// 회원정보 수정 / Put, Patch 
+		// 회원정보 수정 / Put, Patch
 		return mv;
 	}
-	
+
 //	@PostMapping
 	public ModelAndView deleteMember(ModelAndView mv) {
 		// 회원탈퇴 / DeleteMapping
 		return mv;
 	}
-	
+
 	@ExceptionHandler(Exception.class)
 	public ModelAndView memberExceptionHandler(Exception e
-			/* 오류발생함 ModelAndView mv */) {
+	/* 오류발생함 ModelAndView mv */) {
 		e.printStackTrace();
-		
+
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("msg", e.getMessage() + "오류가 발생했습니다. 다시 시도해주세요.");
 		mv.setViewName("error/error500");
-		
+
 		return mv;
 	}
 
 }
- 

45e26929157e4f5eed0393bd7b46a0a513d5d923
diget611 <diget611@naver.com>
2023-03-28 10:57:00 +0900

    관리자 권한 페이지 추가

--- src/main/java/kh/spring/chazazo/admin/controller/AdminController.java
+++ src/main/java/kh/spring/chazazo/admin/controller/AdminController.java
@@ -1,16 +1,19 @@
 package kh.spring.chazazo.admin.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("/admin")
 public class AdminController {
 	
 	// 전부 다른 테이블에서 끌어오거나 하니까 adminservice 같은건 필요없을거 같고. 다른 서비스들 주워와서 쓰면 될듯?
 	
-//	@GetMapping
+	@GetMapping("/main")
 	public ModelAndView viewStatistics(ModelAndView mv) {
-		// 통계 조회용
+		mv.setViewName("admin/main");
 		return mv;
 	}
 
